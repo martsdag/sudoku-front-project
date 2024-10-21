78dbbc6a63755ce32b605ae6592fffa2d1d28994
martsdag <clarte.lisse.31@gmail.com>
2024-10-21 19:02:20 +0400

    + Add difficulty parameter validation, integrate difficulty from route query in ViewSudoku

--- src/views/Sudoku/ViewSudoku.vue
+++ src/views/Sudoku/ViewSudoku.vue
@@ -6,14 +6,21 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { useRoute } from 'vue-router';
 
-import { Difficulty, type Sudoku } from '@/api/sudoku';
+import { type Sudoku } from '@/api/sudoku';
 import { useSudokuStore } from '@/stores/sudoku';
+import { isDifficulty } from '@/helpers/isDifficulty';
 
+const route = useRoute();
 const sudokuStore = useSudokuStore();
 const model = ref<Sudoku['puzzle']>([]);
 
-sudokuStore.getSudoku(Difficulty.Easy).then((sudoku) => {
+if (!isDifficulty(route.query.difficulty)) {
+  throw new Error('Введите сложность');
+}
+
+sudokuStore.getSudoku(route.query.difficulty).then((sudoku) => {
   model.value = sudoku.puzzle;
 });
 </script>
