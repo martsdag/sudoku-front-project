c4487d5870b2a00669ad37d4a3ceb66d2dfac1fa
martsdag <clarte.lisse.31@gmail.com>
2024-10-23 19:54:42 +0400

    refactor: improved color palette

--- src/components/BaseButton/BaseButton.vue
+++ src/components/BaseButton/BaseButton.vue
@@ -1,25 +1,5 @@
 <template>
-  <button type="button" class="button" :class="{ 'button:disabled': disabled }" :disabled @click="onClick">
-    <div class="button-content">
-      <slot />
-    </div>
+  <button type="button" class="button">
+    <slot />
   </button>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const emit = defineEmits<{
-  click: [Event];
-}>();
-
-const disabled = ref(false);
-
-const onClick = (e: Event) => {
-  if (disabled.value) {
-    return;
-  }
-
-  emit('click', e);
-};
-</script>
