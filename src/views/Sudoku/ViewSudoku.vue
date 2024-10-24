ebc015411ece0f97ccd700304ea83da30062b61a
martsdag <clarte.lisse.31@gmail.com>
2024-10-24 15:40:19 +0400

    Fix difficulty validation and handling for getting sudoku

--- src/views/Sudoku/ViewSudoku.vue
+++ src/views/Sudoku/ViewSudoku.vue
@@ -1,27 +1,53 @@
 <template>
   <div>
     <div>модель: {{ model }}</div>
+    <div class="button-container">
+      <RouterLink
+        v-for="difficulty in Object.values(Difficulty)"
+        :to="{ name: RouteName.Sudoku, query: { difficulty } }"
+        class="button"
+        :key="difficulty"
+      >
+        {{ difficulty }}
+      </RouterLink>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 
-import { type Sudoku } from '@/api/sudoku';
+import { Difficulty, type Sudoku } from '@/api/sudoku';
 import { useSudokuStore } from '@/stores/sudoku';
 import { isDifficulty } from '@/helpers/isDifficulty';
 import { goToPage404 } from '@/composables/goToPage404';
+import { RouteName } from '@/router';
 
 const route = useRoute();
 const sudokuStore = useSudokuStore();
 const model = ref<Sudoku['puzzle']>([]);
 
-if (!isDifficulty(route.query.difficulty)) {
-  goToPage404();
-} else {
-  sudokuStore.getSudoku(route.query.difficulty).then((sudoku) => {
-    model.value = sudoku.puzzle;
-  });
-}
+watch(
+  () => route.query.difficulty,
+  () => {
+    const difficulty = route.query.difficulty;
+
+    if (!isDifficulty(difficulty)) {
+      return goToPage404();
+    }
+
+    sudokuStore.getSudoku(difficulty).then((sudoku) => {
+      model.value = sudoku.puzzle;
+    });
+  },
+  { immediate: true },
+);
 </script>
+
+<style>
+.button-container {
+  display: flex;
+  gap: 10px;
+}
+</style>
