c3c1509f89b3c20265c576683f4876ebb069576a
martsdag <clarte.lisse.31@gmail.com>
2024-10-28 16:51:11 +0400

    + sudoku grid

--- src/views/Sudoku/ViewSudoku.vue
+++ src/views/Sudoku/ViewSudoku.vue
@@ -11,6 +11,22 @@
         {{ difficulty }}
       </RouterLink>
     </div>
+    <table class="sudoku-grid">
+      <tbody>
+        <tr v-for="(row, rowIndex) in model" :key="rowIndex">
+          <td
+            v-for="(cell, cellIndex) in row"
+            :class="{
+              'bold-border-right': (cellIndex + 1) % 3 === 0 && cellIndex !== 8,
+              'bold-border-bottom': (rowIndex + 1) % 3 === 0 && rowIndex !== 8,
+            }"
+            :key="cellIndex"
+          >
+            {{ cell }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -53,5 +69,34 @@ watch(
     gap: 10px;
     flex-wrap: wrap;
   }
+}
+
+.sudoku-grid {
+  display: flex;
+  justify-content: center;
+  border-collapse: collapse;
+  width: 100%;
+  max-width: 358px;
+  margin: 0 auto;
+  background-color: #ffffff;
+}
+
+td {
+  width: 36px;
+  height: 36px;
+  max-width: 36px;
+  max-height: 36px;
+  border: 1px solid black;
+  text-align: center;
+  vertical-align: middle;
+  font-size: 24px;
+}
+
+.bold-border-right {
+  border-right: 2px solid black;
+}
+
+.bold-border-bottom {
+  border-bottom: 2px solid black;
 }
 </style>
