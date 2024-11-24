76a90e0194ffe0aa72e31560f5bb44ac75903794
martsdag <clarte.lisse.31@gmail.com>
2024-11-25 01:44:05 +0400

    working on footer and header

--- src/layouts/Default/components/LayoutDefaultFooter.vue
+++ src/layouts/Default/components/LayoutDefaultFooter.vue
@@ -1,3 +1,33 @@
 <template>
-  <footer>footer</footer>
+  <footer class="footer">
+    <div class="footer-links">
+      <a target="_blank" :href="telegramLink">
+        <img src="@/assets/icons/telegramLogo.svg" width="24" height="24" alt="Telegram Logo" />
+      </a>
+      <a target="_blank" :href="githubLink">
+        <img src="@/assets/icons/githubLogo.svg" width="24" height="24" alt="Github Logo" />
+      </a>
+    </div>
+  </footer>
 </template>
+
+<script setup lang="ts">
+//почекать поч футер такой гигантский и что можно с этим сделать(проблема решается тем что height: 100dvh;)
+const telegramLink = import.meta.env.VITE_TELEGRAM_LINK;
+const githubLink = import.meta.env.VITE_GITHUB_LINK;
+</script>
+
+<style>
+.footer {
+  border-top: 1px solid var(--color-blue-950);
+}
+.footer-links {
+  width: 90%;
+  margin: 0 auto;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+}
+</style>
--- src/layouts/Default/components/LayoutDefaultHeader.vue
+++ src/layouts/Default/components/LayoutDefaultHeader.vue
@@ -1,3 +1,57 @@
 <template>
-  <header>header</header>
+  <header class="header">
+    <div class="header__content">
+      <RouterLink :to class="header__link header__link--home">На главную</RouterLink>
+      <ul class="header__list">
+        <li v-for="link in links" class="header__item" :key="link.to.name">
+          <RouterLink :to="link.to" activeClass="header__link--active" class="header__link">Судоку</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </header>
 </template>
+
+<script setup lang="ts">
+import { RouteName } from '@/router';
+import { computed } from 'vue';
+
+const to = { name: RouteName.Home };
+const links = computed(() => [{ to: { name: RouteName.Sudoku } }]);
+</script>
+
+<style>
+.header {
+  border-bottom: 1px solid var(--color-blue-950);
+}
+
+.header__content {
+  width: 90%;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+.header__list {
+  display: flex;
+  gap: 0.5rem;
+  padding: 0;
+}
+
+.header__link {
+  color: var(--color-blue-950);
+}
+
+.header__item {
+  list-style-type: none;
+}
+
+.header__link--home {
+  font-weight: lighter;
+}
+
+.header__link--active {
+  text-decoration: underline;
+  font-weight: bold;
+}
+</style>
