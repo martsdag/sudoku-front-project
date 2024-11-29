15fb62479a6533e4a766f69696eaa35f6266a48d
martsdag <clarte.lisse.31@gmail.com>
2024-11-29 15:05:35 +0400

    upd styles

--- src/layouts/Default/components/LayoutDefaultHeader.vue
+++ src/layouts/Default/components/LayoutDefaultHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="header">
     <div class="header__content _container">
-      <ul class="header__links">
-        <li v-for="link in links" class="header__links-link" :key="link.to.name">
+      <ul class="header__links-container">
+        <li v-for="link in links" :key="link.to.name">
           <RouterLink :to="link.to" activeClass="header__link--active" class="header__link">
             {{ link.text }}
           </RouterLink>
@@ -25,52 +25,47 @@ const links = [
 .header {
   border-bottom: 1px solid var(--color-blue-200);
   background-color: var(--color-white);
-}
 
-.header__content {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-}
+  .header__content {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+  .header__links-container {
+    display: flex;
+    gap: 1rem;
+    padding: 0;
+  }
 
-.header__links {
-  display: flex;
-  gap: 1rem;
-  padding: 0;
-}
+  .header__link {
+    color: var(--color-blue-700);
+    text-decoration: none;
+    font-size: 18px;
+    position: relative;
 
-.header__links-link {
-  list-style-type: none;
-}
+    &:hover {
+      color: var(--color-blue-500);
 
-.header__link {
-  color: var(--color-blue-700);
-  text-decoration: none;
-  font-size: 18px;
-  position: relative;
-
-  &:hover {
-    color: var(--color-blue-500);
+      &::after {
+        width: 100%;
+      }
+    }
 
     &::after {
-      width: 100%;
+      content: '';
+      position: absolute;
+      bottom: -3px;
+      left: 0;
+      width: 0;
+      height: 0.125rem;
+      background-color: var(--color-blue-400);
+      transition: width 0.3s ease;
+    }
+
+    &.header__link--active {
+      font-weight: bold;
+      color: var(--color-blue-500);
     }
   }
-
-  &::after {
-    content: '';
-    position: absolute;
-    bottom: -3px;
-    left: 0;
-    width: 0;
-    height: 0.125rem;
-    background-color: var(--color-blue-400);
-    transition: width 0.3s ease;
-  }
-}
-
-.header__link--active {
-  font-weight: bold;
-  color: var(--color-blue-500);
 }
 </style>
