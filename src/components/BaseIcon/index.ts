d2e1fbec42cb87c1ba9a0de3dc33bb0d53ee5e0a
martsdag <clarte.lisse.31@gmail.com>
2024-12-02 20:24:42 +0400

    + BaseIcon component

---
+++ src/components/BaseIcon/index.ts
@@ -0,0 +1,3 @@
+export { default } from './BaseIcon.vue';
+
+export * from './types';
--- src/helpers/ui.ts
+++ src/helpers/ui.ts
@@ -2,3 +2,11 @@ export const BUTTON = Object.freeze({
   default: 'button',
   ACTIVE: 'button_active',
 });
+
+export const ICON = Object.freeze({
+  default: 'icon',
+  SIZE: Object.freeze({
+    SM: 'icon_size_sm',
+    LG: 'icon_size_lg',
+  }),
+});
