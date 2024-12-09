a78eefdb6f2996a52aee6d1f795b3be7ced9c564
martsdag <clarte.lisse.31@gmail.com>
2024-12-09 15:24:30 +0400

    draft: +Base dialog

---
+++ src/components/BaseDialog.vue
@@ -0,0 +1,170 @@
+<template>
+  <dialog class="_container dialog" ref="dialog" @close="onClose" @cancel.prevent="onClose">
+    <div class="dialog__inner" ref="dialogInner">
+      <slot v-if="!isHiddenHeader" name="header" v-bind="{ close }">
+        <div class="dialog__header">
+          <span v-if="title" class="dialog__header-title">{{ title }}</span>
+          <BaseButton class="dialog__button" @click="onClickCloseIcon">
+            <BaseIcon :path="mdiClose" />
+          </BaseButton>
+        </div>
+      </slot>
+
+      <slot v-bind="{ close }" />
+
+      <slot v-if="!isHiddenFooter" name="footer" v-bind="{ close }">
+        <div v-if="buttons.length" class="dialog__footer">
+          <BaseButton v-for="(button, index) in buttons" :key="index" @click="button.onClick">
+            {{ button.text }}
+          </BaseButton>
+        </div>
+      </slot>
+    </div>
+  </dialog>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, onBeforeUnmount, useTemplateRef, type PropType } from 'vue';
+import { onClickOutside, useMutationObserver, useScrollLock, useToggle } from '@vueuse/core';
+import { mdiClose } from '@mdi/js';
+import BaseButton from './BaseButton/BaseButton.vue';
+import BaseIcon from './BaseIcon/BaseIcon.vue';
+
+const dialog = useTemplateRef('dialog');
+const dialogInner = useTemplateRef('dialogInner');
+
+const isLocked = useScrollLock(document.getElementById('main'));
+const toggleIsLocked = useToggle(isLocked);
+
+interface Button {
+  text: string;
+  onClick: () => void;
+}
+
+const props = defineProps({
+  title: String,
+  buttons: {
+    type: Array as PropType<Button[]>,
+    default: () => [],
+  },
+  isHiddenHeader: Boolean,
+  isHiddenFooter: Boolean,
+});
+
+const emit = defineEmits(['open', 'close', 'confirm', 'cancel']);
+
+const buttons = computed(() =>
+  props.buttons.length
+    ? props.buttons
+    : [
+        {
+          text: 'Cancel',
+          onClick: () => {
+            emit('cancel');
+            close();
+          },
+        },
+        {
+          text: 'Confirm',
+          onClick: () => {
+            emit('confirm');
+            close();
+          },
+        },
+      ],
+);
+
+const isOpened = ref(false);
+
+useMutationObserver(
+  dialog,
+  () => {
+    isOpened.value = Boolean(dialog.value?.open);
+  },
+  { attributes: true },
+);
+
+const open = () => {
+  if (isOpened.value) {
+    return;
+  }
+
+  dialog.value?.showModal();
+  toggleIsLocked(true);
+  emit('open');
+};
+
+const close = () => {
+  if (!isOpened.value) {
+    return;
+  }
+
+  dialog.value?.close();
+  toggleIsLocked(false);
+  emit('close');
+};
+
+const onClose = close;
+const onClickCloseIcon = close;
+
+onClickOutside(dialogInner, close);
+
+onBeforeUnmount(() => toggleIsLocked(false));
+
+defineExpose({
+  open,
+  close,
+  isOpened,
+});
+</script>
+
+<style>
+.dialog {
+  border-width: 1px;
+  border-color: var(--color-blue-500);
+  outline: 2px solid transparent;
+  outline-offset: 2px;
+  border-radius: 0.5rem;
+  background-color: var(--color-white);
+  margin: auto !important;
+  overflow-x: hidden;
+  &::backdrop {
+    background-image: linear-gradient(
+      130deg,
+      var(--color-blue-300),
+      var(--color-blue-200) 41.07%,
+      var(--color-blue-100) 76.05%
+    );
+  }
+}
+
+.dialog__inner {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  scrollbar-gutter: stable both-edges;
+}
+
+.dialog__header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1.5rem;
+}
+
+.dialog__header-title {
+  font-size: 18px;
+}
+
+.dialog__button {
+  margin-inline-start: auto;
+}
+
+.dialog__footer {
+  display: flex;
+  justify-content: flex-end;
+  gap: 0.5rem;
+  margin-top: auto;
+}
+</style>
