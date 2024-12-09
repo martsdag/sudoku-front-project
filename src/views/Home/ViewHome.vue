0aacf7ebe8798add19f8edc3bba1911df8354cec
martsdag <clarte.lisse.31@gmail.com>
2024-12-09 18:51:31 +0400

    draft: using the component of base dialog

--- src/views/Home/ViewHome.vue
+++ src/views/Home/ViewHome.vue
@@ -1,3 +1,34 @@
 <template>
-  <div></div>
+  <BaseButton @click="onOpen">Open Dialog</BaseButton>
+  <BaseDialog title="Текст" ref="baseDialog" @open="onOpen" @close="onClose"></BaseDialog>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import BaseButton from '@/components/BaseButton';
+import BaseDialog from '@/components/BaseDialog.vue';
+
+const baseDialog = ref<InstanceType<typeof BaseDialog> | null>(null);
+
+const onOpen = () => {
+  baseDialog.value?.open();
+};
+
+const onClose = () => {
+  baseDialog.value?.close();
+};
+</script>
+
+<style>
+.dialog__button-close {
+  margin-left: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.dialog__p {
+  margin: 15px 0;
+  font-size: 1rem;
+  line-height: 1.4;
+}
+</style>
