b8d83d3a0d605bacea87744429b8946c895329a0
martsdag <clarte.lisse.31@gmail.com>
2024-12-10 15:15:45 +0400

    fix

--- src/views/Home/ViewHome.vue
+++ src/views/Home/ViewHome.vue
@@ -1,30 +1,3 @@
 <template>
-  <BaseButton @click="onOpen">Open Dialog</BaseButton>
-  <BaseDialog ref="baseDialog" @open="onOpen" @close="onClose">
-    <img src="https://avatars.mds.yandex.net/i?id=9413f4a95071e05daff4a079eb7986a5_l-5348026-images-thumbs&n=13"
-  /></BaseDialog>
+  <div></div>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-import BaseButton from '@/components/BaseButton/BaseButton.vue';
-import BaseDialog from '@/components/BaseDialog/BaseDialog.vue';
-
-const baseDialog = ref<InstanceType<typeof BaseDialog> | null>(null);
-
-const onOpen = () => {
-  baseDialog.value?.open();
-};
-
-const onClose = () => {
-  baseDialog.value?.close();
-};
-</script>
-
-<style>
-.dialog__h3 {
-  margin: 15px 0;
-  font-size: 1rem;
-  line-height: 1.4;
-}
-</style>
