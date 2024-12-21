940c16e9b34a201c0eb6f382bb9e5809fd00c0eb
martsdag <clarte.lisse.31@gmail.com>
2024-12-21 14:51:54 +0400

    + modals for sudoku

---
+++ src/components/DialogIsWin.vue
@@ -0,0 +1,20 @@
+<template>
+  <BaseDialog :buttons="[{ id: 0, text: 'OK', onClick: () => closeDialog() }]" ref="baseDialog">
+    <img src="/src/assets/images/victory.jpg" alt="Victory image" />
+  </BaseDialog>
+</template>
+
+<script setup lang="ts">
+import BaseDialog from '@/components/BaseDialog/BaseDialog.vue';
+import { useTemplateRef } from 'vue';
+
+const baseDialog = useTemplateRef('baseDialog');
+
+const openDialog = () => baseDialog.value?.open();
+const closeDialog = () => baseDialog.value?.close();
+
+defineExpose({
+  openDialog,
+  closeDialog,
+});
+</script>
